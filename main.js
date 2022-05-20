1b5f7e273785954419a819eb38f032844407845c
Carlos Alberto López Mata <mata66666666666@gmail.com>
2022-05-20 14:23:03 -0500

    update

--- main.js
+++ main.js
@@ -1,10 +1,35 @@
 const menu = document.querySelector('.menu');
 const btn = document.querySelector('.menu-btn');
-
     btn.addEventListener('click', () => {
         menu.classList.toggle('active')
     })
- 
+
+const menu1 = document.querySelector('.menu1');
+    menu1.addEventListener('click', () => {
+        menu.classList.toggle('active')
+    })
+const menu2 = document.querySelector('.menu2');
+menu2.addEventListener('click', () => {
+    menu.classList.toggle('active')
+})
+const menu3 = document.querySelector('.menu3');
+menu3.addEventListener('click', () => {
+    menu.classList.toggle('active')
+})
+const menu4 = document.querySelector('.menu4');
+menu4.addEventListener('click', () => {
+    menu.classList.toggle('active')
+})
+const menu5 = document.querySelector('.menu5');
+menu5.addEventListener('click', () => {
+    menu.classList.toggle('active')
+})
+const menu6 = document.querySelector('.menu6');
+menu6.addEventListener('click', () => {
+    menu.classList.toggle('active')
+})
+                        
+         
 document.addEventListener('DOMContentLoaded', () => {
     const imgLightBox = document.querySelectorAll('materialboxed');
     M.Materialbox.init(imgLightBox);
